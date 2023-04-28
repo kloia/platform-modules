a35a375bf01778e630925485df3273d5bd494c36
ugurcan.caykara <ugurcan.caykara@kloia.com>
2023-04-28 14:27:41 +0300

    adjust waf ip set module to feed waf module

---
+++ aws-waf-ip-set/main.tf
@@ -0,0 +1,19 @@
+resource "aws_wafv2_ip_set" "blacklist_ip_set" {
+
+  ip_address_version = "IPV4"
+  scope              = "CLOUDFRONT"
+  description        = var.blacklist_ip_set_description
+  name               = var.blacklist_ip_set_name
+  addresses          = var.blacklist_ip_addresses
+
+
+}
+
+resource "aws_wafv2_ip_set" "whitelist_ip_set" {
+  ip_address_version = "IPV4"
+  scope              = "CLOUDFRONT"
+  description        = var.ip_set_description
+  name               = var.ip_set_name
+  addresses          = var.ip_set_addresses
+
+}
