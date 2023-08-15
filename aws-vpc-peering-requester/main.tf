24d7c5de6b70b4154942548be3db3a5513734761
ugurcan.caykara <ugurcan.caykara@kloia.com>
2023-08-15 11:55:44 +0300

    add cross account functionality

--- aws-vpc-peering-requester/main.tf
+++ aws-vpc-peering-requester/main.tf
@@ -1,4 +1,6 @@
 resource "aws_vpc_peering_connection" "peer" {
+    count = length(var.requester_private_route_table_ids) > 0 ? 1 : 0
+
     peer_owner_id = var.peer_owner_account_id #requester account id
     peer_vpc_id = var.accepter_vpc_id #accepter vpc id
     vpc_id = var.requester_vpc_id #requester vpc id
@@ -9,3 +11,98 @@ resource "aws_vpc_peering_connection" "peer" {
         Name = "aws peering - requester"
     }
 }
+
+
+
+
+
+### Cross account requester resources 
+
+
+# Upstream account values
+data "aws_ssm_parameter" "requester_private_route_table_ids" {
+  count = var.create_cross_account_peering ? 1 : 0
+  provider = aws.shared_infra
+  name = var.requester_private_route_table_ids_key
+}
+
+data "aws_ssm_parameter" "requester_public_route_table_ids" {
+  count = var.create_cross_account_peering ? 1 : 0
+  provider = aws.shared_infra
+  name = var.requester_public_route_table_ids_key
+}
+
+data "aws_ssm_parameter" "requester_vpc_cidr_block" {
+  count = var.create_cross_account_peering ? 1 : 0
+  provider = aws.shared_infra
+  name = var.requester_vpc_cidr_block_key
+}
+
+data "aws_ssm_parameter" "requester_vpc_id" {
+  count = var.create_cross_account_peering ? 1 : 0
+  provider = aws.shared_infra
+  name = var.requester_vpc_id_key
+}
+
+
+
+
+# VPC Peering Connection Options
+
+resource "aws_vpc_peering_connection" "cross_peer" {
+    provider = aws.shared_infra
+    count = var.create_cross_account_peering ? 1 : 0
+
+    vpc_id = data.aws_ssm_parameter.requester_vpc_id[0].value #requester vpc id
+    peer_vpc_id = var.accepter_vpc_id #accepter vpc id
+    peer_owner_id = var.peer_owner_account_id #accepter account id
+    peer_region = var.peer_region
+    auto_accept = false
+
+    tags = {
+        Name = "aws peering - requester"
+    }
+
+}
+
+# Accepter's side of the connection.
+resource "aws_vpc_peering_connection_accepter" "cross_accepter" {
+  count = var.create_cross_account_peering ? 1 : 0
+
+  vpc_peering_connection_id = aws_vpc_peering_connection.cross_peer[0].id
+  auto_accept               = true
+
+  tags = {
+    Side = "Accepter"
+  }
+}
+
+
+
+
+resource "aws_vpc_peering_connection_options" "cross_peer" {
+  count = var.create_cross_account_peering ? 1 : 0
+
+  provider = aws.shared_infra
+
+  # As options can't be set until the connection has been accepted
+  # create an explicit dependency on the accepter.
+  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.cross_accepter[0].id
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
+
+resource "aws_vpc_peering_connection_options" "cross_accepter" {
+  count = var.create_cross_account_peering ? 1 : 0
+
+
+  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.cross_accepter[0].id
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
+
+
--- aws-vpc-peering-requester/variables.tf
+++ aws-vpc-peering-requester/variables.tf
@@ -59,3 +59,54 @@ variable "peer_region" {
   type        = string
   default     = ""
 }
+
+
+variable "enable_cross_account_peering" {
+  description = "Control logic to create cross account peering not cross region"
+  type = string
+  default = ""
+}
+
+variable "create_peering" {
+  description = "Regional VPC Peering creation control logic"
+  type = bool
+  default = true
+}
+
+
+
+
+
+# Cross account variables
+
+variable "create_cross_account_peering" {
+  description = "Cross account VPC Peering creation control variable"
+  type = bool
+  default = false
+}
+
+
+# For SSM Data sources
+variable "requester_private_route_table_ids_key" {
+  description = "Cross account requester private route table ids SSM Key"
+  type = string
+  default = ""
+}
+
+variable "requester_public_route_table_ids_key" {
+  description = "Cross account requester private route table ids SSM Key"
+  type = string
+  default = ""
+}
+
+variable "requester_vpc_cidr_block_key" {
+  description = "Cross account requester vpc cidr block SSM Key"
+  type = string
+  default = ""
+}
+
+variable "requester_vpc_id_key" {
+  description = "Cross account requester vpc id SSM Key"
+  type = string
+  default = ""
+}
