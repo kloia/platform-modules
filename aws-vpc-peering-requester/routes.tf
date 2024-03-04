62162e8ed9047c11874703f9a2c766739cc94e40
Halil Bozan <halilbozan@MacBook-Pro-3.local>
2024-03-04 09:47:00 +0300

    requester peering module updated.

--- aws-vpc-peering-requester/routes.tf
+++ aws-vpc-peering-requester/routes.tf
@@ -1,5 +1,5 @@
 resource "aws_route" "requester_private_route_table_route" {
-    count = length(var.requester_private_route_table_ids) > 0 ? length(var.requester_private_route_table_ids) : 0
+    count = var.create_cross_account_peering ? 0 : 1
     vpc_peering_connection_id = aws_vpc_peering_connection.peer[0].id
     route_table_id = var.requester_private_route_table_ids[count.index]
     destination_cidr_block = var.accepter_vpc_cidr_block
@@ -8,7 +8,7 @@ resource "aws_route" "requester_private_route_table_route" {
 
 
 resource "aws_route" "requester_public_route_table_route" {
-    count = length(var.requester_public_route_table_ids) > 0 ? length(var.requester_public_route_table_ids) : 0
+    count = var.create_cross_account_peering ? 0 : 1
     vpc_peering_connection_id = aws_vpc_peering_connection.peer[0].id
     route_table_id = var.requester_public_route_table_ids[count.index]
     destination_cidr_block = var.accepter_vpc_cidr_block
@@ -20,9 +20,9 @@ resource "aws_route" "requester_public_route_table_route" {
 # Cross Account Route Resources
 # Upstream (Requester Account) Routes
 locals {
-    private_route_table_ids = var.create_cross_account_peering ? try(jsondecode(data.aws_ssm_parameter.requester_private_route_table_ids[0].value),null) : null
+    private_route_table_ids = var.create_cross_account_peering ? var.requester_private_route_table_ids: null
 
-    public_route_table_ids = var.create_cross_account_peering ?  try(jsondecode(data.aws_ssm_parameter.requester_public_route_table_ids[0].value),null) : null
+    public_route_table_ids = var.create_cross_account_peering ?  var.requester_public_route_table_ids: null
 }
 
 
@@ -50,7 +50,7 @@ resource "aws_route" "cross_account_accepter_private_route_table_route" {
     count = length(var.accepter_private_route_table_ids) > 0 && var.create_cross_account_peering ? length(var.accepter_private_route_table_ids) : 0
     vpc_peering_connection_id = aws_vpc_peering_connection.cross_peer[0].id
     route_table_id = var.accepter_private_route_table_ids[count.index]
-    destination_cidr_block = try(data.aws_ssm_parameter.requester_vpc_cidr_block[0].value,null)
+    destination_cidr_block = var.requester_vpc_cidr_block
 
 }
 
@@ -59,7 +59,7 @@ resource "aws_route" "cross_account_accepter_public_route_table_route" {
     count = length(var.accepter_public_route_table_ids) > 0 && var.create_cross_account_peering ? length(var.accepter_public_route_table_ids) : 0
     vpc_peering_connection_id = aws_vpc_peering_connection.cross_peer[0].id
     route_table_id = var.accepter_public_route_table_ids[count.index]
-    destination_cidr_block = try(data.aws_ssm_parameter.requester_vpc_cidr_block[0].value,null)
+    destination_cidr_block = var.requester_vpc_cidr_block
 
 }
 
