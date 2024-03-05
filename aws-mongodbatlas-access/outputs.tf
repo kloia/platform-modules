2074be18e9eb1d74942a470e3bd26368b1b1feb3
ömer faruk urhan <omerurhan2@gmail.com>
2024-03-05 12:57:42 +0300

    fix mongoatlas access output

--- aws-mongodbatlas-access/outputs.tf
+++ aws-mongodbatlas-access/outputs.tf
@@ -1,10 +1,10 @@
-output "atlas_assumed_role_external_id" {
-    value = mongodbatlas_cloud_provider_access_setup.cloud_provider_access_role.aws.atlas_assumed_role_external_id
-}
-
-output "atlas_aws_account_arn" {
-    value = mongodbatlas_cloud_provider_access_setup.cloud_provider_access_role.aws.atlas_aws_account_arn
-}
+#output "atlas_assumed_role_external_id" {
+#    value = mongodbatlas_cloud_provider_access_setup.cloud_provider_access_role.aws.atlas_assumed_role_external_id
+#}
+#
+#output "atlas_aws_account_arn" {
+#    value = mongodbatlas_cloud_provider_access_setup.cloud_provider_access_role.aws.atlas_aws_account_arn
+#}
 
 output "atlas_role_id" {
     value = mongodbatlas_cloud_provider_access_setup.cloud_provider_access_role.role_id
