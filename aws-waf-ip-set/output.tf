e8fa1b57161a637b732e1489be03fc376c8ca86a
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-03-18 22:49:47 +0800

    adjust output definition

--- aws-waf-ip-set/output.tf
+++ aws-waf-ip-set/output.tf
@@ -8,9 +8,8 @@ output "whitelist_ip_set_arn" {
   description = "arn of whitelist ip set"
 }
 
-output "dynamic_waf_ip_set_arn" {
-
-  value       = aws_wafv2_ip_set.ip_addresses.arn
-  description = "arns of dynamically created ip address groups"
+output "dynamic_waf_ip_set_arns" {
+  value       = { for key, ip_set in aws_wafv2_ip_set.ip_addresses : key => ip_set.arn }
+  description = "Map of ARNs for dynamically created IP address groups"
 }
 
