6f0d468532c3a805dde1e164e0fa14d6631a40d6
akcinardoga <165195458+akcinardoga@users.noreply.github.com>
2025-07-03 17:39:10 +0300

    feat(eks-addons): internal alb output value added if exists (#270)

--- modules/aws-eks-addons/outputs.tf
+++ modules/aws-eks-addons/outputs.tf
@@ -12,3 +12,8 @@ output "load_balancer_hostname" {
   description = "Load balancer Hostname"
   value       = kubernetes_ingress_v1.alb_ingress_connect_nginx.0.status.0.load_balancer.0.ingress.0.hostname
 }
+
+output "internal_load_balancer_hostname" {
+  description = "Internal load balancer Hostname"
+  value       = var.enable_internal_alb ? kubernetes_ingress_v1.alb_ingress_connect_nginx_internal.0.status.0.load_balancer.0.ingress.0.hostname : null
+}
