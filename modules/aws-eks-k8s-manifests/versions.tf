86ce0e8998bfe53b8c32e322850eb267c33efda1
kholisrag <4683704+kholisrag@users.noreply.github.com>
2026-01-27 08:51:30 +0700

    fix: add aws to required_providers

--- modules/aws-eks-k8s-manifests/versions.tf
+++ modules/aws-eks-k8s-manifests/versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 3.0.0, < 4.0.0"
