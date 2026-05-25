509b1e65e755e294367ef5aacd7bf0d68294f6ac
Muhammad Bagus Ramadhan <bagus.ramadan@kloia.com>
2026-05-25 14:27:09 +0700

    fix(aws-glue): fix aws glue module (#328)

--- modules/aws-glue/main.tf
+++ modules/aws-glue/main.tf
@@ -1,8 +1,6 @@
-module "glue_catalog_database" {
-  source  = "terraform-aws-modules/glue/aws//modules/catalog-database"
-  version = "~> 1.0"
+resource "aws_glue_catalog_database" "this" {
+  count = var.create_database ? 1 : 0
 
-  create       = var.create_database
   name         = var.database_name
   description  = var.database_description
   catalog_id   = var.catalog_id
@@ -12,13 +10,11 @@ module "glue_catalog_database" {
   tags = var.tags
 }
 
-module "glue_crawler" {
-  source  = "terraform-aws-modules/glue/aws//modules/crawler"
-  version = "~> 1.0"
+resource "aws_glue_crawler" "this" {
+  count = var.create_crawler ? 1 : 0
 
-  create        = var.create_crawler
   name          = var.crawler_name
-  database_name = var.create_database ? module.glue_catalog_database.name : var.database_name
+  database_name = var.create_database ? aws_glue_catalog_database.this[0].name : var.database_name
   role          = var.crawler_role_arn
   description   = var.crawler_description
   schedule      = var.crawler_schedule
@@ -26,11 +22,28 @@ module "glue_crawler" {
   classifiers   = var.crawler_classifiers
   configuration = var.crawler_configuration
 
-  s3_targets           = var.s3_targets
-  schema_change_policy = var.schema_change_policy
-  recrawl_policy       = var.recrawl_policy
+  dynamic "s3_target" {
+    for_each = var.s3_targets
+    content {
+      path                = s3_target.value.path
+      exclusions          = try(s3_target.value.exclusions, [])
+      connection_name     = s3_target.value.connection_name
+      event_queue_arn     = s3_target.value.event_queue_arn
+      dlq_event_queue_arn = s3_target.value.dlq_event_queue_arn
+      sample_size         = s3_target.value.sample_size
+    }
+  }
+
+  schema_change_policy {
+    update_behavior = try(var.schema_change_policy.update_behavior, "UPDATE_IN_DATABASE")
+    delete_behavior = try(var.schema_change_policy.delete_behavior, "LOG")
+  }
+
+  recrawl_policy {
+    recrawl_behavior = try(var.recrawl_policy.recrawl_behavior, "CRAWL_EVERYTHING")
+  }
 
   tags = var.tags
 
-  depends_on = [module.glue_catalog_database]
+  depends_on = [aws_glue_catalog_database.this]
 }
